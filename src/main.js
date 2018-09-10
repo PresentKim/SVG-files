4da5f47ee8ebf9dbc11bf0068cfcc552768e5616
PresentKim <qjrmckd@gmail.com>
2018-09-11 03:58:19 +0900

    Install `vue-router`

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import 'babel-polyfill'
 import 'event-source-polyfill'
 
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+Vue.use(VueRouter);
 
 new Vue({
 	el: '#app',
-	render: h => h(App)
+	render: h => h(App),
+	router: new VueRouter()
 });
