876c1c199e75f6b6f13e317bb16503d1362b8260
PresentKim <debe3721@gmail.com>
2020-02-22 15:50:15 +0900

    Show custom context menu on svg + Ommit 'v-bind'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
 	<div id="app">
 		<div id="filters">
-			<div v-for="newFilter in filters" v-bind:key="newFilter" :class="{active: filter === newFilter}" class="filter" @click="filter = newFilter">{{ newFilter }}</div>
+			<div v-for="newFilter in filters" :key="newFilter" :class="{active: filter === newFilter}" class="filter" @click="filter = newFilter">{{ newFilter }}</div>
 		</div>
 		<div id="svgs">
-			<div v-for="svg in svgs" v-bind:key="svg.src" :class="{hide: filter !== `all` && !svg.tags.includes(filter)}" v-html="svg.src"></div>
+			<div v-for="svg in svgs" :key="svg.src" :class="{hide: filter !== `all` && !svg.tags.includes(filter)}" v-html="svg.src" @contextmenu="openMenu"></div>
 		</div>
 		<div id="github">
 			<a href="https://github.com/PresentKim/SVG-files/">View SVG</a>
 			|
 			<a href="https://github.com/PresentKim/SVG-files/tree/gh-pages">View Source</a>
+		</div>
+
+		<div id="contextmenu" tabindex="-1" v-if="viewMenu" ref="right" @blur="closeMenu" :style="{top: top, left: left}">
+			<li>Download to SVG</li>
+			<li>Download to PNG</li>
 		</div>
 	</div>
 </template>
@@ -22,7 +27,10 @@
 		data() {
 			return {
 				filters: [`all`],
-				svgs: []
+				svgs: [],
+				viewMenu: false,
+				top: `0px`,
+				left: `0px`
 			}
 		},
 		computed: {
@@ -56,6 +64,24 @@
 					}
 				};
 			});
+		},
+		methods: {
+			openMenu: function(event) {
+				this.viewMenu = true;
+				this.svgElement = event.path.find(element => element.tagName == `svg`);
+				this.$nextTick(function() {
+					this.$refs.right.focus();
+					let largestHeight = window.innerHeight - this.$refs.right.offsetHeight;
+					let largestWidth = window.innerWidth - this.$refs.right.offsetWidth;
+					this.top = `${event.y > largestHeight ? largestHeight : event.y}px`;
+					this.left = `${event.x > largestWidth ? largestWidth : event.x}px`;
+				}.bind(this));
+				event.preventDefault();
+			},
+			closeMenu: function() {
+				this.viewMenu = false;
+				this.svgElement = null;
+			}
 		}
 	}
 </script>
@@ -135,6 +161,26 @@
 					&:hover {
 						border-top: 3pt solid $nord8;
 						border-bottom: 3pt solid $nord8;
+					}
+				}
+			}
+
+			#contextmenu{
+				background: $nord1;
+				border: 1px solid $nord4;
+				display: block;
+				list-style: none;
+				margin: 0;
+				padding: 0;
+				position: absolute;
+				width: 200px;
+				z-index: 999999;
+
+				li {
+					margin: 0;
+					padding: 5px 30px;
+					&:hover {
+						background: $nord3;
 					}
 				}
 			}
